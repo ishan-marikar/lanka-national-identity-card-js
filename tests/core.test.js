0f30601389eaff49590147f4950e6683289a881e
Ishan Marikar <ishan@ranomark.com>
2018-05-21 11:56:55 +0530

    Fix bug with NIC holders with birthdays on January

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,27 +1,50 @@
 const lankaNIC = require("../");
-const NIC = "951524255V";
 
-describe("NIC Utilities", () => {
+// These NIC numbers are randomly generated.
+// Any resemblance to that of an actual person's is purely coincidental.
+const NIC1 = "925182566V";
+const NIC2 = "951484366V";
+
+describe("NIC (Female - 1992.01.18)", () => {
   test("should return correct type for date of birth", () => {
-    let { dateOfBirth } = lankaNIC.getInfoFromNIC(NIC);
+    let { dateOfBirth } = lankaNIC.getInfoFromNIC(NIC1);
     expect(dateOfBirth).toBeInstanceOf(Date);
   });
 
   test("should return correct type for gender", () => {
-    let { gender } = lankaNIC.getInfoFromNIC(NIC);
+    let { gender } = lankaNIC.getInfoFromNIC(NIC1);
+    expect(typeof gender).toBe("string");
+  });
+
+  test("should return correct response for gender", () => {
+    let { gender } = lankaNIC.getInfoFromNIC(NIC1);
+    expect(gender).toBe("female");
+  });
+
+  test("should return correct response for date of birth", () => {
+    let { dateOfBirth } = lankaNIC.getInfoFromNIC(NIC1);
+    expect(dateOfBirth).toEqual(new Date("1992-01-18"));
+  });
+});
+
+describe("NIC (Male - 1995.05.27)", () => {
+  test("should return correct type for date of birth", () => {
+    let { dateOfBirth } = lankaNIC.getInfoFromNIC(NIC2);
+    expect(dateOfBirth).toBeInstanceOf(Date);
+  });
+
+  test("should return correct type for gender", () => {
+    let { gender } = lankaNIC.getInfoFromNIC(NIC2);
     expect(typeof gender).toBe("string");
   });
 
   test("should return the correct gender for the NIC number", () => {
-    let { gender } = lankaNIC.getInfoFromNIC(NIC);
-    expect(gender).toBe("m");
+    let { gender } = lankaNIC.getInfoFromNIC(NIC2);
+    expect(gender).toBe("male");
   });
 
   test("should return correct response for date of birth", () => {
-    let { dateOfBirth } = lankaNIC.getInfoFromNIC(NIC);
-    // In Javascript, months start from the index 0
-    // https://coderwall.com/p/txgk3w/javascript-dates-month-weirdness
-    // https://stackoverflow.com/questions/2552483/why-does-the-month-argument-range-from-0-to-11-in-javascripts-date-constructor
-    https: expect(dateOfBirth).toEqual(new Date(1995, 5 - 1, 31));
+    let { dateOfBirth } = lankaNIC.getInfoFromNIC(NIC2);
+    expect(dateOfBirth).toEqual(new Date("1995-05-27"));
   });
 });
